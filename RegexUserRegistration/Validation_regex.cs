8ef37bdb67bfd68e38b697025fb9326ea2b0756f
Ankush Bahekar <ankushmb9@gmail.com>
2023-07-17 19:33:19 +0530

    [Ankush]Add:Validate Last name.

--- RegexUserRegistration/Validation_regex.cs
+++ RegexUserRegistration/Validation_regex.cs
@@ -7,13 +7,21 @@ using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
-    internal class Validation_regex
+    public class Validation_regex
     {
         public void Checkfirstname(string firstname)
         {
             string Firstnamepattern = "^[A-Z][a-z]{3,10}$";
             if (Regex.IsMatch(firstname, Firstnamepattern))
                 Console.WriteLine("First Name is valid");
+            else
+                Console.WriteLine("Invalid name!!");
+        }
+        public void Checklastname(string Lastname)
+        {
+            string lastnamepattern = "^[A-Z][a-z]{3,10}$";
+            if (Regex.IsMatch(Lastname, lastnamepattern))
+                Console.WriteLine("Last Name is valid");
             else
                 Console.WriteLine("Invalid name!!");
         }
